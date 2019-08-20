11b8a5324b8654685d051ebcab1e9cc9335231db
hatemosphere <hatemosphere@protonmail.com>
2019-08-20 16:46:31 +0300

    Make Helm chart index idempotent 3

--- package/package.sh
+++ package/package.sh
@@ -19,8 +19,10 @@ helm init --client-only
 helm repo add "${CHART_REPO_NAME}" "${CHART_REPO_URL}"
 helm repo add jetstack https://charts.jetstack.io/
 
-mkdir -p "${BUILD_DIR}"
+# Creating /BUILD_TEMP_DIR/ dir
+BUILD_TEMP_DIR=$(mktemp -d)
 
+# Packaging new charts to /BUILD_TEMP_DIR/
 for chart in ./stable/*; do
   chart_version=$(grep version "${chart}"/Chart.yaml | awk '{print $2}')
   set +e
@@ -30,21 +32,21 @@ for chart in ./stable/*; do
       echo "--- Skipping chart ${chart%/} ${chart_version} as it already exists in ${CHART_REPO_URL}"
     else
       set -e
-      echo "--- Packaging ${chart} into ${BUILD_DIR}"
+      echo "--- Packaging ${chart} into ${BUILD_TEMP_DIR}"
       helm dep update "${chart}" || true
-      helm package --destination "${BUILD_DIR}" "${chart}"
+      helm package "${chart}" --destination "${BUILD_TEMP_DIR}"
     fi
 done
-ls "${BUILD_DIR}"
+ls "${BUILD_TEMP_DIR}"
 
 git clone "https://${GITHUB_USER:-$DEFAULT_GITHUB_USER}:${GITHUB_TOKEN}@github.com/${GIT_REPO}" out
 pushd out
 git checkout "${TARGET_BRANCH}" || git checkout --orphan "${TARGET_BRANCH}"
-cp index.yaml "${BUILD_DIR}" || true
+cp index.yaml "${BUILD_TEMP_DIR}" || true
 popd
 
-pushd "${BUILD_DIR}"
-echo "--- Reindexing ${BUILD_DIR}"
+pushd "${BUILD_TEMP_DIR}"
+echo "--- Reindexing ${BUILD_TEMP_DIR}"
 if [ -f index.yaml ]; then
   helm repo index --url "${CHART_REPO_URL}" --merge index.yaml .
 else
@@ -52,4 +54,5 @@ else
 fi
 popd
 
-cp "${BUILD_DIR}"/* out/
+cp -r "${BUILD_TEMP_DIR}"/* out/
+cp -r out/* "${BUILD_DIR}"
