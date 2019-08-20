395208ca02eafa6505102150aa986f4570eb1f4e
Ihor Borodin <hatemosphere@protonmail.com>
2019-08-20 15:16:18 +0300

    Do not repackage already existing charts 3 (#19)
    * Do not repackage already existing charts 3

--- package/package.sh
+++ package/package.sh
@@ -4,16 +4,19 @@
 
 set -euo pipefail
 
-HELM_REPO_URL="$1"
-GIT_REPO="$2"
-GITHUB_TOKEN="$3"
-BUILD_DIR="$4"
+CHART_REPO_NAME="$1"
+CHART_REPO_URL="$2"
+GIT_REPO="$3"
+GITHUB_TOKEN="$4"
+BUILD_DIR="$5"
+
 DEFAULT_GITHUB_USER=koalificationio-bot
 TARGET_BRANCH=gh-pages
 
 helm init --client-only
 
-# add helm repos for charts in requirements
+# add own helm chart repo and repos for charts in requirements
+helm repo add "${CHART_REPO_NAME}" "${CHART_REPO_URL}"
 helm repo add jetstack https://charts.jetstack.io/
 
 git clone "https://${GITHUB_USER:-$DEFAULT_GITHUB_USER}:${GITHUB_TOKEN}@github.com/${GIT_REPO}" "${BUILD_DIR}"
@@ -25,9 +28,9 @@ for chart in ./stable/*; do
   chart_version=$(grep version "${chart}"/Chart.yaml | awk '{print $2}')
   set +e
     chart_basename=$(basename "${chart}")
-    helm inspect "${HELM_REPO_URL}/${chart_basename}" --version "${chart_version}" > /dev/null
+    helm inspect "${CHART_REPO_NAME}/${chart_basename}" --version "${chart_version}" > /dev/null
     if [[ "$?" -eq "0" ]]; then
-      notify "Skipping chart ${chart%/} ${chart_version} as it already exists in ${HELM_REPO_URL}"
+      notify "Skipping chart ${chart%/} ${chart_version} as it already exists in ${CHART_REPO_URL}"
     else
       set -e
       echo "--- Packaging ${chart} into ${BUILD_DIR}"
@@ -41,8 +44,8 @@ ls "${BUILD_DIR}"
 pushd "${BUILD_DIR}"
 echo "--- Reindexing ${BUILD_DIR}"
 if [ -f index.yaml ]; then
-  helm repo index --url "${HELM_REPO_URL}" --merge index.yaml .
+  helm repo index --url "${CHART_REPO_URL}" --merge index.yaml .
 else
-  helm repo index --url "${HELM_REPO_URL}" .
+  helm repo index --url "${CHART_REPO_URL}" .
 fi
 popd
