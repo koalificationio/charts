326aff8ddc2a4aa75c37ba478e42debec35f9081
hatemosphere <hatemosphere@protonmail.com>
2019-08-20 12:49:08 +0300

    Fixing git clone chart repo 4

--- package/package.sh
+++ package/package.sh
@@ -26,11 +26,11 @@ ls "${BUILD_DIR}"
 
 pushd "${BUILD_DIR}"
 git clone "https://${GITHUB_USER:-$DEFAULT_GITHUB_USER}:${GITHUB_TOKEN}@github.com/${GIT_REPO}" out
-pushd "${BUILD_DIR}/out"
+pushd out
 git checkout "${TARGET_BRANCH}" || git checkout --orphan "${TARGET_BRANCH}"
 popd
 
-cp out/index.yaml "${BUILD_DIR}" || true
+cp out/index.yaml . || true
 echo "--- Reindexing ${BUILD_DIR}"
 if [ -f index.yaml ]; then
   helm repo index --url "${HELM_REPO_URL}" --merge index.yaml .
